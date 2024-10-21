5de82a078e1697613c4e55f5918c9a91e3dd06a9
Tyler <tyler.whitchurch@atlasschool.com>
2024-10-20 20:21:20 -0500

    py

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -14,56 +14,62 @@ from models.review import Review
 
 
 class DBStorage:
-    """ This class manages storage of hbnb models in a database """
+    """This class manages storage of hbnb models in a database"""
     __engine = None
     __session = None
 
     def __init__(self):
-        """ Constructor for DBStorage """
-        self.__engine = create_engine('mysql+mysqldb://{}:{}@{}/{}'
-                                      .format(getenv('HBNB_MYSQL_USER'),
-                                              getenv('HBNB_MYSQL_PWD'),
-                                              getenv('HBNB_MYSQL_HOST'),
-                                              getenv('HBNB_MYSQL_DB')),
-                                      pool_pre_ping=True)
+        """Constructor for DBStorage"""
+        self.__engine = create_engine(
+            'mysql+mysqldb://{}:{}@{}/{}'.format(
+                getenv('HBNB_MYSQL_USER'),
+                getenv('HBNB_MYSQL_PWD'),
+                getenv('HBNB_MYSQL_HOST'),
+                getenv('HBNB_MYSQL_DB')
+            ),
+            pool_pre_ping=True
+        )
         if getenv('HBNB_ENV') == 'test':
             Base.metadata.drop_all(self.__engine)
 
     def all(self, cls=None):
-        """ Returns a dictionary of models currently in storage """
+        """Returns a dictionary of models currently in storage"""
         from models import classes
         objs = {}
         if cls:
-            if type(cls) == str:
+            if isinstance(cls, str):
                 cls = classes[cls]
             for obj in self.__session.query(cls):
-                objs[obj.__class__.__name__ + '.' + obj.id] = obj
+                key = f"{obj.__class__.__name__}.{obj.id}"
+                objs[key] = obj
         else:
             for name, cls in classes.items():
                 for obj in self.__session.query(cls):
-                    objs[obj.__class__.__name__ + '.' + obj.id] = obj
+                    key = f"{obj.__class__.__name__}.{obj.id}"
+                    objs[key] = obj
         return objs
 
     def new(self, obj):
-        """ Adds new object to storage """
+        """Adds new object to storage"""
         self.__session.add(obj)
 
     def save(self):
-        """ Saves storage to database """
+        """Saves storage to database"""
         self.__session.commit()
 
     def delete(self, obj=None):
-        """ Deletes obj from storage """
+        """Deletes obj from storage"""
         if obj:
             self.__session.delete(obj)
 
     def reload(self):
-        """ Reloads storage from database """
+        """Reloads storage from database"""
         Base.metadata.create_all(self.__engine)
-        session_factory = sessionmaker(bind=self.__engine, expire_on_commit=False)
+        session_factory = sessionmaker(bind=self.__engine,
+                                       expire_on_commit=False)
         Session = scoped_session(session_factory)
         self.__session = Session()
 
     def close(self):
-        """ Calls remove() on the private session attribute """
+        """Calls remove() on the private session attribute"""
         self.__session.close()
