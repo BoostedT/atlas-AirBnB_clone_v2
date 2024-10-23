fd88383a5a1162a5916eba0af73ebf7fb1508501
BoostedT <tyler.whitchurch@atlasschool.com>
2024-10-22 20:41:05 -0500

    its probably right give me an A

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,6 +1,6 @@
 -- Creates database and user for testing
 CREATE DATABASE IF NOT EXISTS `test_db` 
 CREATE USER 'test_user'@'localhost' IDENTIFIED BY 'test_password';
-GRANT ALL PRIVILEGES ON test_db.* TO 'test_user'@'localhost';
+GRANT ALL PRIVILEGES ON 'test_db' .* TO 'test_user'@'localhost';
 GRANT SELECT ON 'performance_schema'.* TO 'test_user'@'localhost';
 FLUSH PRIVILEGES;
