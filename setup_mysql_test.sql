a3134478b8e3637660c7fc55d77deaa756463b88
BoostedT <tyler.whitchurch@atlasschool.com>
2024-10-22 20:42:28 -0500

    changed test to hbnb

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,6 +1,6 @@
 -- Creates database and user for testing
-CREATE DATABASE IF NOT EXISTS `test_db` 
-CREATE USER 'test_user'@'localhost' IDENTIFIED BY 'test_password';
-GRANT ALL PRIVILEGES ON 'test_db' .* TO 'test_user'@'localhost';
-GRANT SELECT ON 'performance_schema'.* TO 'test_user'@'localhost';
+CREATE DATABASE IF NOT EXISTS hbnb_test_db;
+CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
+GRANT ALL PRIVILEGES ON `hbnb_test_db`.* TO 'hbnb_test'@'localhost';
+GRANT SELECT ON `performance_schema`.* TO 'hbnb_test'@'localhost';
 FLUSH PRIVILEGES;
