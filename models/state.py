6bf9d1094140fdc290a9ab3e626a329d9357a3d5
Tyler <tyler.whitchurch@atlasschool.com>
2024-10-23 14:42:00 -0500

    1

--- models/state.py
+++ models/state.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import Base
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 import models
 
-class State(BaseModel, Base):
+class State(Base):
     """ State class """
     __tablename__ ='states'
     name = Column(String(128), nullable=False)
