6279edcafa39872138c4244fbfde996f0aac3117
clay <rpctropic13@gmail.com>
2024-10-28 21:40:02 -0500

    added primary key for state

--- models/state.py
+++ models/state.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
-""" State Module for HBNB project """
-from models.base_model import Base
-from sqlalchemy import Column, String
+from models.base_model import Base, BaseModel
+from sqlalchemy import Column, String, Integer
 from sqlalchemy.orm import relationship
 import models
 
-class State(Base):
+class State(BaseModel, Base):
     """ State class """
     __tablename__ ='states'
     __table_args__ = {'extend_existing': True}
+    id = Column(Integer, primary_key=True, autoincrement=True)
     name = Column(String(128), nullable=False)
     cities = relationship("City", backref="state", cascade="all, delete")
 
