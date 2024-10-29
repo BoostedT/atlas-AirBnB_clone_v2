f6b1846d8fb6398b51b7181e1b6aa3610b626640
clay <rpctropic13@gmail.com>
2024-10-29 18:43:16 -0500

    simplied the init bc the dictionary was relocated

--- models/__init__.py
+++ models/__init__.py
@@ -1,15 +1,7 @@
 #!/usr/bin/python3
-""" init file for models """
+"""This module instantiates an object of class FileStorage"""
+from models.engine.file_storage import FileStorage
+from models.classes import classes
 
-import os
-
-storage_type = os.getenv("HBNB_TYPE_STORAGE")
-
-if storage_type == "db":
-    from models.engine.db_storage import DBStorage
-    storage = DBStorage()
-else:
-    from models.engine.file_storage import FileStorage
-    storage = FileStorage()
-
-storage.reload()  # Load the storage system
+storage = FileStorage()
+storage.reload()
