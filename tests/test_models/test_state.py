228e56bc74b76f95c40c96331f6b9db9aa6967a9
zack6yuan <zack6yuan@gmail.com>
2024-10-29 21:08:40 -0500

    update state test.

--- tests/test_models/test_state.py
+++ tests/test_models/test_state.py
@@ -5,15 +5,15 @@ from models.state import State
 
 
 class test_state(test_basemodel):
-    """ """
+    """ Method: Create class for state test """
 
     def __init__(self, *args, **kwargs):
-        """ """
+        """ Method: Constrcutor """
         super().__init__(*args, **kwargs)
         self.name = "State"
         self.value = State
 
     def test_name3(self):
-        """ """
+        """ Method: test name-type(str) """
         new = self.value()
         self.assertEqual(type(new.name), str)
