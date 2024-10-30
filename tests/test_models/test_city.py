f2fd3c6d1c3a381e0b8e9f97132738c6629d522a
zack6yuan <zack6yuan@gmail.com>
2024-10-29 21:01:16 -0500

    update city test.

--- tests/test_models/test_city.py
+++ tests/test_models/test_city.py
@@ -5,20 +5,20 @@ from models.city import City
 
 
 class test_City(test_basemodel):
-    """ """
+    """ Method: Create city test class """
 
     def __init__(self, *args, **kwargs):
-        """ """
+        """ Method: constructor """
         super().__init__(*args, **kwargs)
         self.name = "City"
         self.value = City
 
     def test_state_id(self):
-        """ """
+        """ Method: Verify state_id-type(str)"""
         new = self.value()
         self.assertEqual(type(new.state_id), str)
 
     def test_name(self):
-        """ """
+        """ Method: Verify name-type(str)"""
         new = self.value()
         self.assertEqual(type(new.name), str)
