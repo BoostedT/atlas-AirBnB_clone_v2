dbb8db531a8bdfa87ca1415ad6c2f858edcf56db
zack6yuan <zack6yuan@gmail.com>
2024-10-29 20:40:44 -0500

    update amenity.

--- models/amenity.py
+++ models/amenity.py
@@ -5,5 +5,10 @@ from sqlalchemy import Column, String
 
 
 class Amenity(BaseModel):
+    """ Method: initialize table representing amenities"""
     __tablename__ = 'amenities'
     name = Column(String(128), nullable=False)
+    
+def __init__(self, *args, **kwargs):
+    """ Initialize amenity """
+    super().__init__(*args, **kwargs)
