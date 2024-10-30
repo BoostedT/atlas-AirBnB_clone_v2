fb9469172150c16277e87d39e91e49fac0ab2925
zack6yuan <zack6yuan@gmail.com>
2024-10-29 21:07:53 -0500

    update review test.

--- tests/test_models/test_review.py
+++ tests/test_models/test_review.py
@@ -14,16 +14,16 @@ class test_review(test_basemodel):
         self.value = Review
 
     def test_place_id(self):
-        """ Method: Verify"""
+        """ Method: Test place_id-type(str) """
         new = self.value()
         self.assertEqual(type(new.place_id), str)
 
     def test_user_id(self):
-        """ """
+        """ Method: Test user_id-type(str) """
         new = self.value()
         self.assertEqual(type(new.user_id), str)
 
     def test_text(self):
-        """ """
+        """ Method: Test text-type(str) """
         new = self.value()
         self.assertEqual(type(new.text), str)
