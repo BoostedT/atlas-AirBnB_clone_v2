6c2040449c91f6c4adf5ad084fcf62e7980d766e
zack6yuan <zack6yuan@gmail.com>
2024-10-29 21:09:46 -0500

    update user test.

--- tests/test_models/test_user.py
+++ tests/test_models/test_user.py
@@ -5,30 +5,30 @@ from models.user import User
 
 
 class test_User(test_basemodel):
-    """ """
+    """ Method: Create class for user test """
 
     def __init__(self, *args, **kwargs):
-        """ """
+        """ Method: Constructor """
         super().__init__(*args, **kwargs)
         self.name = "User"
         self.value = User
 
     def test_first_name(self):
-        """ """
+        """ Method: Test first_name-type(str) """
         new = self.value()
         self.assertEqual(type(new.first_name), str)
 
     def test_last_name(self):
-        """ """
+        """ Method: Test last_name-type(str) """
         new = self.value()
         self.assertEqual(type(new.last_name), str)
 
     def test_email(self):
-        """ """
+        """ Method: Test email-type(str) """
         new = self.value()
         self.assertEqual(type(new.email), str)
 
     def test_password(self):
-        """ """
+        """ Method: Test password-type(str) """
         new = self.value()
         self.assertEqual(type(new.password), str)
