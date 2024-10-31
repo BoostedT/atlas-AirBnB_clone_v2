efbde795701cb4f0f186609fa5d82ac75c3ee51f
Tyler <tyler.whitchurch@atlasschool.com>
2024-10-31 17:29:38 -0500

    yut

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -66,3 +66,7 @@ class DBStorage:
         Base.metadata.create_all(self.__engine)
         session_factory = sessionmaker(bind=self.__engine, expire_on_commit=False)
         Session = scoped_session(session_factory)
+
+    def close(self):
+        """Calls remove method on the private session attribute"""
+        self.__session.remove()
