d63f9c724677439dc513a49b482184cf566ae1dd
Tyler <tyler.whitchurch@atlasschool.com>
2024-11-01 20:59:56 -0500

    fixed state and hopefully circualar import

--- models/state.py
+++ models/state.py
@@ -3,8 +3,6 @@ from models.base_model import Base, BaseModel
 from sqlalchemy import Column, String, Integer
 from sqlalchemy.orm import relationship
 import models
-from models import storage
-from models.city import City
 
 class State(BaseModel, Base):
     """ State class """
@@ -12,11 +10,15 @@ class State(BaseModel, Base):
     __table_args__ = {'extend_existing': True}
     id = Column(Integer, primary_key=True, autoincrement=True)
     name = Column(String(128), nullable=False)
-    cities = relationship("City", backref="state", cascade="all, delete")
+    
+    # Only use the relationship when DBStorage is the engine
+    if models.storage.__class__.__name__ == 'DBStorage':
+        cities = relationship("City", backref="state", cascade="all, delete")
 
     @property
     def cities(self):
-        """ Getter for cities """
-        if storage.__class__.__name__ != 'DBStorage':
-            return [city for city in storage.all(City).values() if city.state_id == self.id]
+        """ Getter for cities if using FileStorage """
+        if models.storage.__class__.__name__ != 'DBStorage':
+            from models.city import City  # Import inside the method to avoid circular import
+            return [city for city in models.storage.all(City).values() if city.state_id == self.id]
         return []
